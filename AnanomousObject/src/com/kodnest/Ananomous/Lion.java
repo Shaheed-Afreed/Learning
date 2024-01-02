30791580d8e9a13cc1792788d081a529da5f99a5
Shaheed-Afreed <141918061+Shaheed-Afreed@users.noreply.github.com>
2024-01-02 10:22:24 +0530

    Add files via upload

---
+++ AnanomousObject/src/com/kodnest/Ananomous/Lion.java
@@ -0,0 +1,16 @@
+package com.kodnest.Ananomous;
+
+public class Lion {
+	void King()
+	{
+		System.out.println("Lion is King of Jungle");
+	}
+	
+
+	public static void main(String[] args) {
+		new Lion().King();
+		
+
+	}
+
+}
