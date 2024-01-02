d8895fd08b61a6ceeb65678b06b92aeab22c7ece
Shaheed-Afreed <141918061+Shaheed-Afreed@users.noreply.github.com>
2024-01-02 10:21:39 +0530

    Add files via upload

---
+++ Alphabet/src/com/kodnest/Alphabet/Alphabet.java
@@ -0,0 +1,20 @@
+package com.kodnest.Alphabet;
+
+public class Alphabet {
+
+	public static void main(String[] args) {
+for(int i=65;i<=90;i++)
+{
+	char ch=(char) i;
+	System.out.print(ch+" |");
+}
+System.out.println();
+for(int i=97;i<=122;i++)
+{
+	char ch=(char) i;
+	System.out.print(ch+" |");
+}
+
+	}
+
+}
